9fd65ebbbff94630e93328552181e983c4e4d04e
DanielDeveloper <developer.danielalves@gmail.com>
2024-03-03 15:13:56 -0300

    quest new

--- atividade1.js
+++ atividade1.js
@@ -16,9 +16,18 @@ console.log(`Ola! ${nome}`);
 
 // console.log(`A soma de ${valor1} + ${valor2} é igual a ${resultado}`)
 
-var valor1 = 10;
-var valor2 = 5;
+// var valor1 = 10;
+// var valor2 = 5;
 
-var resultado = valor1 - valor2;
+// var resultado = valor1 - valor2;
 
-console.log(`A diferença entre os números ${valor1} e ${valor2} é ${resultado}`)
+// console.log(`A diferença entre os números ${valor1} e ${valor2} é ${resultado}`)
+
+var idade = prompt('Qual sua idade? ')
+
+if(idade >= 18){
+    console.log('Voce é de maior de idade!')
+} else{
+    console.log('Você é menor e idade!')
+}
+
