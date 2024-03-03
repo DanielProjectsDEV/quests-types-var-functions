003227292c3f36d027cf8ebf63ed034393fa1b25
DanielDeveloper <developer.danielalves@gmail.com>
2024-03-03 14:25:10 -0300

    atividades

---
+++ atividade1.js
@@ -0,0 +1,24 @@
+console.log(`Welcome`);
+
+var nome = 'Daniel';
+
+console.log(`Ola! ${nome}`);
+
+// alert(`Ola ${nome}`);
+
+// var linguagem = prompt('Qual é a sua linguagem de programção que você mais gosta? ');
+// alert(linguagem + ' é uma ótima linguagem de programação!');
+
+// var valor1 = 2;
+// var valor2 = 4;
+
+// var resultado = valor1 + valor2
+
+// console.log(`A soma de ${valor1} + ${valor2} é igual a ${resultado}`)
+
+var valor1 = 10;
+var valor2 = 5;
+
+var resultado = valor1 - valor2;
+
+console.log(`A diferença entre os números ${valor1} e ${valor2} é ${resultado}`)
